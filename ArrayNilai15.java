2572df43032215780946103eb977d208de8cbeef
A1liiii <mhmmdalimirza@gmail.com>
2024-11-01 08:45:26 +0700

    modifikas program ArrrayNilai sesuai dengan no 1,3&4

--- ArrayNilai15.java
+++ ArrayNilai15.java
@@ -5,15 +5,19 @@ public class ArrayNilai15 {
         Scanner sc = new Scanner(System.in);  
         int[] nilaiAkhir = new int[10];  
 
-        for (int i = 0; i < 10; i++) {  
-            System.out.print("Masukkan nilai akhir ke-" + (i) + " : ");  
+        for (int i = 0; i < nilaiAkhir.length; i++) {  
+            System.out.print("Masukkan nilai akhir ke-" + i + " : ");  
             nilaiAkhir[i] = sc.nextInt();  
-        }  
+        } 
 
-        System.out.println("\nDaftar nilai akhir:");  
-        for (int i = 0; i < 10; i++) {  
-            System.out.println("Nilai akhir ke-" + (i) + " adalah " + nilaiAkhir[i]);  
-        }  
+        System.out.println("\nStatus Kelulusan:");  
+        for (int i = 0; i < nilaiAkhir.length; i++) {  
+            if (nilaiAkhir[i] > 70) {  
+                System.out.println("Mahasiswa ke-" + (i) + " lulus!");  
+            } else {  
+                System.out.println("Mahasiswa ke-" + (i) + " tidak lulus!");  
+            }  
+        }   
 
         sc.close();
     }  
